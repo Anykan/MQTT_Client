a5d2d8612c6490bc691a2e39c420c2535fe7032f
Anykan <Anykan@users.noreply.github.com>
2022-07-20 13:27:02 +0200

    Update mqtt_client.py

--- mqtt_client.py
+++ mqtt_client.py
@@ -10,7 +10,8 @@ config.read('config.ini')
 
 MQTT_SERVER = config['mqtt']['server']
 MQTT_PATH = config['mqtt']['path']
-print(MQTT_SERVER)
+LED_ANZ = int(config['led']['led'])
+LED_ZONEN = int(config['led']['zonen'])
 # WZ Decke
 # 0-145 hinten
 # 146-295 rechts (Fenster)
@@ -20,80 +21,37 @@ print(MQTT_SERVER)
 # 0-107 rechts TV
 # 108-242
 # 243-350
-pixels = neopixel.NeoPixel(board.D18, 4)
+pixels = neopixel.NeoPixel(board.D18, LED_ANZ)
 encoding = 'utf-8'
-rgb = [0,0,0,0,0,0,0,0,0,0,0,0,] # 0-2 Farbe1,3-5 Farbe2, 6-8 Farbe3, 9-11 Farbe4
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
-    pixels.fill((0,0,0))
+ 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
     client.subscribe(MQTT_PATH)
  
 # The callback for when a PUBLISH message is received from the server.
-
-def on_message(client, userdata, message):
-    decoded_message=str(message.payload.decode("utf-8"))
-    msg=json.loads(decoded_message)
-    anzahlZonen = msg['Zonen']
-#    tmp = msg['Farbe1']
-#   r1 = str(tmp[0]) + str(tmp[1])
-#   g1 = str(tmp[2]) + str(tmp[3])
-#   b1 = str(tmp[4]) + str(tmp[5])
-#    print(r1)
-#    print(g1)
-#    print(b1)
-    if anzahlZonen == "1":
-        print("1 Zone")
-        rgb_werte(msg['Farbe1'])
-        print("Rot: " + str(rgb[0]))
-        print("Gruen: " + str(rgb[1]))
-        print("Blau: " + str(rgb[2]))     
-        pixels[0]= (rgb[0],rgb[1],rgb[2])
-    if anzahlZonen == "2":
-        print("2 Zonen")
-        rgb_werte(msg['Farbe1'])
-        pixels[0]= (rgb[0],rgb[1],rgb[2])
-        rgb_werte(msg['Farbe2'])
-        pixels[1]= (rgb[0],rgb[1],rgb[2])
-
-    if anzahlZonen == "3":
-        print("3 Zonen")
-        rgb_werte(msg['Farbe1'])
-        pixels[0]= (rgb[0],rgb[1],rgb[2])
-        rgb_werte(msg['Farbe2'])
-        pixels[1]= (rgb[0],rgb[1],rgb[2])
-        rgb_werte(msg['Farbe3'])
-        pixels[2]= (rgb[0],rgb[1],rgb[2])
-    if anzahlZonen == "4":
-        print("4 Zonen")
-        rgb_werte(msg['Farbe1'])
-        pixels[0]= (rgb[0],rgb[1],rgb[2])
-        rgb_werte(msg['Farbe2'])
-        pixels[1]= (rgb[0],rgb[1],rgb[2])
-        rgb_werte(msg['Farbe3'])
-        pixels[2]= (rgb[0],rgb[1],rgb[2])
-        rgb_werte(msg['Farbe4'])
-        pixels[3]= (rgb[0],rgb[1],rgb[2])
-
-def rgb_werte(hex_farbe):
+def on_message(client, userdata, msg):
     prefix ="0x"
-    rgb[0] = int(prefix + hex_farbe[0] + hex_farbe [1],0)
-    rgb[1] = int(prefix + hex_farbe[2] + hex_farbe [3],0)
-    rgb[2] = int(prefix + hex_farbe[4] + hex_farbe [5],0)
-    return rgb
+    color = prefix + str(msg.payload,encoding)
+    red_hex = prefix + color[2] + color[3]
+    green_hex = prefix + color[4] + color[5]
+    blue_hex = prefix + color[6] + color[7]
+    red=int(red_hex,0)
+    green=int(green_hex,0)
+    blue=int(blue_hex,0) 
+    pixels.fill((red, green, blue))
     
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 client.will_set(MQTT_PATH + "/LWT", payload="Offline")
-client.connect(MQTT_SERVER, 1883, 30)
+client.connect(MQTT_SERVER, 1883, 10)
  
 # Blocking call that processes network traffic, dispatches callbacks and
 # handles reconnecting.
 # Other loop*() functions are available that give a threaded interface and a
 # manual interface.
-
 client.publish(MQTT_PATH + "/LWT","Online")
 client.loop_forever()
